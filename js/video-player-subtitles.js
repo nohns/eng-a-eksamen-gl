569dfb26085e8a0d25002175f275378bb88f4be5
Asger Nohns <asgernohns@Asgers-MacBook-Pro.local>
2020-01-07 13:11:23 +0100

    Initial

---
+++ js/video-player-subtitles.js
@@ -0,0 +1,187 @@
+var subtitles = {
+    T_181381_trump_america_first: {
+		lang: "en",
+		name: "English",
+        tracks: [
+			//from		To			Text
+			T(TTime("00:00:00:01"), TTime("00:00:05:00"), "Kun tekst, der er relevant for opgavebesvarelsen, er tekstet til talesyntese."),
+			T(TTime("00:00:05:00"), TTime("00:00:09:00"), "CNBC"),
+			T(TTime("00:00:09:00"), TTime("00:00:11:15"), "Trump and the cost of ‘America First’"),
+			T(TTime("00:00:11:15"), TTime("00:00:15:00"), "January 20, 2017"),
+			T(TTime("00:00:27:45"), TTime("00:00:30:00"), "Liberal World Order"),
+			T(TTime("00:00:35:30"), TTime("00:00:39:15"), "America led in creating those arrangements"),
+			T(TTime("00:00:42:15"), TTime("00:00:44:15"), "WTO"),
+			T(TTime("00:00:44:15"), TTime("00:00:46:00"), "IMF"),
+			T(TTime("00:00:46:00"), TTime("00:00:47:30"), "NATO"),
+			T(TTime("00:00:47:30"), TTime("00:00:49:15"), "TPP"),
+			T(TTime("00:00:49:15"), TTime("00:00:51:00"), "G-20"),
+			T(TTime("00:00:51:00"), TTime("00:00:51:30"), "G-7"),
+			T(TTime("00:00:51:30"), TTime("00:00:53:15"), "UN"),
+			T(TTime("00:00:53:15"), TTime("00:00:54:20"), "APEC"),
+			T(TTime("00:00:54:20"), TTime("00:00:55:30"), "Shelter zone"),
+			T(TTime("00:00:55:30"), TTime("00:00:58:30"), "The USA won the Cold War with the USSR"),
+			T(TTime("00:01:02:00"), TTime("00:01:08:15"), "The World’s Economy, 2018, 1960 (source: The World Bank)"),
+			T(TTime("00:01:08:15"), TTime("00:01:13:00"), "The U.S. economy is nearly 6x larger"),
+			T(TTime("00:01:13:00"), TTime("00:01:17:15"), "The U.S. spends $14 billion a year"),
+			T(TTime("00:01:17:15"), TTime("00:01:22:20"), "The U.S. has derived hundreds of billions in benefits"),
+			T(TTime("00:01:22:20"), TTime("00:01:24:15"), "Trade expansion"),
+			T(TTime("00:01:24:15"), TTime("00:01:26:15"), "Productivity improvements"),
+			T(TTime("00:01:26:15"), TTime("00:01:27:30"), "Conflict prevention"),
+			T(TTime("00:01:27:30"), TTime("00:01:30:00"), "Safeguards for the global financial system")
+			
+        ]
+    },
+	
+	T_181381_the_trump_tariffs: {
+		lang: "en",
+		name: "English",
+        tracks: [
+			//from		To			Text
+			T(TTime("00:00:00:01"), TTime("00:00:03:45"), "Kun tekst, der er relevant for opgavebesvarelsen, er tekstet til talesyntese."),
+			T(TTime("00:00:03:45"), TTime("00:00:06:00"), "BREAKING NEWS, TRUMP ANNOUNCING NEW TARIFFS AGAINST CHINA, CNN 12:46 PM ET, INSIDE POLITICS"),
+			T(TTime("00:00:11:15"), TTime("00:00:15:30"), "TARIFFS ARE ADDITIONAL TAXES ON IMPORTED GOODS"),
+			T(TTime("00:00:15:30"), TTime("00:00:20:30"), "DRIVES UP THE PRICE OF FOREIGN GOODS"),
+			T(TTime("00:00:20:30"), TTime("00:00:28:00"), "TARIFFS ALLOW DOMESTIC PRODUCTS A BETTER CHANCE TO COMPETE IN THE OPEN MARKET"),
+			T(TTime("00:00:28:00"), TTime("00:00:36:15"), "BREAKING NEWS, TRUMP ANNOUNCING NEW TARIFFS AGAINST CHINA"),
+			T(TTime("00:00:36:15"), TTime("00:00:44:30"), "TARIFFS OFTEN MEAN RETALIATION & TRADE WAR"),
+			T(TTime("00:00:44:30"), TTime("00:00:47:00"), "CONSUMERS FEEL THE COST"),
+			T(TTime("00:00:47:00"), TTime("00:00:50:15"), "WHAT AFFECTS YOUR SHOPPING LIST?"),
+			T(TTime("00:00:50:15"), TTime("00:00:54:30"), "WASHING MACHINES HAVE SHOT UP 17%"),
+			T(TTime("00:00:54:30"), TTime("00:00:58:30"), "25% TARIFF ON STEEL & 10% ON ALUMINUM"),
+			T(TTime("00:00:58:30"), TTime("00:01:04:30"), "EVERYTHING FROM SOUP CANS TO CARS USE IMPORTED STEEL"),
+			T(TTime("00:01:08:15"), TTime("00:01:12:30"), "COCA-COLA IS BEING FORCED TO RAISE PRICES"),
+			T(TTime("00:01:12:30"), TTime("00:01:20:15"), "TRUMP’S STATED HE MIGHT INSTITUTE A SEPARATE 25% FOREIGN CAR TARIFF"),
+			T(TTime("00:01:20:15"), TTime("00:01:26:30"), "Peterson Institute for International Economics<br />COULD DRIVE CAR PRICES UP $1,400 TO $7,000"),
+			T(TTime("00:01:26:30"), TTime("00:01:32:15"), "A FORD ESCAPE, MADE WITH 40% FOREIGN PARTS"),
+			T(TTime("00:01:32:15"), TTime("00:01:36:30"), "PRICE COULD INCREASE BY $2,274"),
+			T(TTime("00:01:36:30"), TTime("00:01:42:30"), "A MERCEDES-BENZ GLC-CLASS, MADE WITH 100% FOREIGN PARTS"),
+			T(TTime("00:01:42:30"), TTime("00:01:50:30"), "PRICE COULD INCREASE TO $45,400"),
+			T(TTime("00:01:50:30"), TTime("00:02:01:15"), "TARIFFS ON CHINESE GOODS COULD RAISE THE PRICE OF APPLE WATCHES, FITBITS, SONOS SPEAKERS & OTHER TECH"),
+			T(TTime("00:02:03:45"), TTime("00:02:09:15"), "TRUMP’S PLANNED OR THREATENED TARIFFS ON THE MAJORITY OF CHINESE GOODS"),
+			T(TTime("00:02:09:15"), TTime("00:02:21:15"), "AMERICAN SPENDING ON CHINESE GOODS: $84B ON CELL PHONES, $67B ON COMPUTERS, AND $28B ON TOYS"),
+			T(TTime("00:02:21:15"), TTime("00:02:26:30"), "TRADE DISPUTES WITH MEXICO, CANADA, & CHINA"),
+			T(TTime("00:02:26:30"), TTime("00:02:34:20"), "BEEF AND PORK OVERSUPPLY"),
+			T(TTime("00:02:34:20"), TTime("00:02:39:15"), "TRUMP BELIEVES THE U.S. WILL COME OUT ON TOP"),
+			T(TTime("00:02:39:15"), TTime("00:02:43:30"), "BUT IF OTHER COUNTRIES CONTINUE TO MATCH WITH THEIR OWN TARIFFS"),
+			T(TTime("00:02:43:30"), TTime("00:02:46:00"), "AMERICANS WILL FEEL IT EVEN DEEPER IN THEIR WALLETS")
+		 ]
+    },
+	
+    
+	T_181381_trumps_trade_wars_explained: {
+		lang: "en",
+		name: "English",
+        tracks: [
+			//from		To			Text
+			T(TTime("00:00:00:01"), TTime("00:00:02:45"), "Kun tekst, der er relevant for opgavebesvarelsen, er tekstet til talesyntese.<br />China raised tariffs by 25%? Yes or No"),
+			T(TTime("00:00:02:45"), TTime("00:00:05:30"), "Robert B Reich and Inequality Media present: TRADE WARS – is not a game –"),
+			T(TTime("00:00:24:15"), TTime("00:00:26:00"), "1"),
+			T(TTime("00:00:26:00"), TTime("00:00:27:30"), "Boeing GM"),
+			T(TTime("00:00:34:30"), TTime("00:00:40:15"), "Foreign–owned enterprises employed nearly 7 million people in the U.S. in 2015 – PEW RESEARCH"),
+			T(TTime("00:00:41:30"), TTime("00:00:46:20"), "BMW manufacturing continues as largest U.S. automotive exporter – BMW of North America"),
+			T(TTime("00:00:46:20"), TTime("00:00:47:30"), "2"),
+			T(TTime("00:00:58:30"), TTime("00:01:06:00"), "Over 146,000 Americans will lose their jobs because of Trump’s trade tariffs"),
+			T(TTime("00:01:06:00"), TTime("00:01:13:30"), "Chip Makers: We’ll End Up Paying Tariffs on Our Own Goods"),
+			T(TTime("00:01:25:15"), TTime("00:01:33:15"), "NPR: China Announces Retaliatory Tariffs on $60 Billion in U.S. Goods"),
+			T(TTime("00:01:33:15"), TTime("00:01:39:30"), "EU weighs tariffs on bourbon, blue jeans, Harley-Davidson (The Hill)"),
+			T(TTime("00:01:39:30"), TTime("00:01:42:15"), "Harley- Davidson, Blaming E.U. Tariffs, Will Move Some Production Out of U.S. (The New York Times, June 25, 2018)"),
+			T(TTime("00:01:46:15"), TTime("00:01:50:15"), "THE SMOOT-HAWLEY TARIFFS HELPED CONTRIBUTE TO THE ECONOMIC HARDSHIPS OF THE GREAT DEPRESSION (Businessinsider.com) "),			
+			T(TTime("00:01:59:30"), TTime("00:02:01:15"), "Invest in job skills"),
+			T(TTime("00:02:01:15"), TTime("00:02:04:00"), "(The Now) Milwaukee named a \‘TechHire City\’"),
+			T(TTime("00:02:12:30"), TTime("00:02:15:45"), "Invest in Research"),
+			T(TTime("00:02:25:15"), TTime("00:02:34:00"), "Invest in Infrastructure"),
+			T(TTime("00:02:34:00"), TTime("00:02:37:00"), "Invest in Healthcare")
+        ]
+    }
+};
+
+/**
+ * Takes up to 4 values 
+ * 4 values: [0]hours[1]minutes[2]seconds[3]frames
+ * 3 values: [0]minutes[1]seconds[2]frames
+ * 2 values: [0]seconds[1]frames
+ * 1 value : [0]frames
+ */
+function ATime() {
+	var frames = 0;
+	var seconds = 0;
+	var minutes = 0;
+	var hours = 0;
+	if(arguments.length == 1) {
+		frames = (arguments[0]*1)/24;
+	}
+	else if(arguments.length == 2) {
+		frames = (arguments[1]*1)/24;
+		seconds = arguments[0]*1;
+	}
+	else if(arguments.length == 3) {
+		frames = (arguments[2]*1)/24;
+		seconds = arguments[1]*1;
+		minutes = (arguments[0]*1)*60;
+	}
+	else if(arguments.length == 4) {
+		frames = (arguments[3]*1)/24;
+		seconds = arguments[2]*1;
+		minutes = (arguments[1]*1)*60;
+		hours = (arguments[0]*1)*3600;
+	}
+	else {
+		return 0;
+	}
+	return hours + minutes + seconds + frames;
+}
+
+/**
+ * If value is undefined returns 0
+ * @param {*} value 
+ */
+function UndefinedToZero(value) {
+	if(typeof value === "undefined") {
+		return 0;
+	}
+	return value;
+}
+
+/**
+ * Convert input to seconds
+ * @param {numeric} frames Not Required
+ * @param {numeric} seconds Not Required
+ * @param {numeric} minutes Not Required
+ * @param {numeric} hours Not Required
+ */
+function _Time(frames, seconds, minutes, hours) {
+	frames = UndefinedToZero(frames) / 24;
+	seconds = UndefinedToZero(seconds);
+	minutes = UndefinedToZero(minutes) * 60;
+	hours = UndefinedToZero(hours) * 3600;
+	return hours + minutes + seconds + frames;
+}
+
+/**
+ * Convert string input "00:00:00:00" to seconds
+ * @param {string} value Required
+ */
+function TTime(value) {
+	if(typeof value === "undefined") {
+		value = "00:00:00:00";
+	}
+	var split = value.split(":");
+	var frames = (split[3]*1)/24;
+	var seconds = split[2]*1;
+	var minutes = (split[1]*1)*60;
+	var hours = (split[0]*1)*3600;
+	var output = hours + minutes + seconds + frames;
+	return output;
+}
+
+function T(start, end, text, direction) {
+	if(typeof direction === "undefined") {
+		direction = "ltr";
+	}
+	return {
+		start: start,
+		end: end,
+		text: text,
+		direction: direction
+	};
+}
